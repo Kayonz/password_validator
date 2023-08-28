7b550c8804543d7d84ad79b26b6e7c84deb699ef
unknown <vitorlara261@gmail.com>
2023-08-27 21:36:50 -0300

    first commit

---
+++ Script.js
@@ -0,0 +1,40 @@
+document.getElementById("password").addEventListener("input", passwordValidate);
+let passwords = [];
+function savePassword(e){
+    const a = document.getElementById('password').value;
+    passwords.push(a);
+}
+function passwordValidate(e) {
+    const a = e.target.value;
+    var Number = hasNumber(a);
+    var UpperCase = hasUpperCase(a);
+    var LowerCase = hasLowerCase(a);
+    var TheMinimumValue = hasTheMinimumValue(a);
+    var SpecialsChars = hasSpecialsChars(a);
+    var newPassword = isNewPassword(a);
+    var test = Number && UpperCase && LowerCase && TheMinimumValue && SpecialsChars & newPassword;
+    console.log(passwords)
+    if (test)
+        document.getElementById('resultado').innerText = "A Senha esta valida!"
+    else
+        document.getElementById('resultado').innerText = "A Senha esta invalida!"
+}
+
+function hasNumber(password) {
+    return /\d/.test(password);
+}
+function hasUpperCase(password) {
+    return /[A-Z]/.test(password);
+}
+function hasLowerCase(password) {
+    return /[a-z]/.test(password);
+}
+function hasTheMinimumValue(password) {
+    return password.length >= 10;
+}
+function hasSpecialsChars(password) {
+    return /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password);
+}
+function isNewPassword(password) {
+    return !passwords.includes(password);
+}
